62f6b6fb9592cbc3f22a35eccaa8d679a3c40fbb
5mudg3r <smudger06@gmail.com>
2016-12-22 15:10:04 +0000

    print rota can now print blank schedule

--- src/me/smudja/PrintRotaHandler.java
+++ src/me/smudja/PrintRotaHandler.java
@@ -5,6 +5,7 @@ import java.awt.Font;
 import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.geom.Line2D;
 import java.awt.print.PageFormat;
 import java.awt.print.Printable;
 import java.awt.print.PrinterException;
@@ -39,16 +40,30 @@ public class PrintRotaHandler implements Printable {
 			return NO_SUCH_PAGE;
 		}
 		
-		pageFormat.setOrientation(PageFormat.LANDSCAPE);
-		
 		Graphics2D g2d = (Graphics2D) graphics;
 		g2d.translate(pageFormat.getImageableX(), pageFormat.getImageableY());
 		
 		graphics.setFont(font);
 		
-		for(int i = 0, i < 9; i++) {
-			g2d.drawLine(x1, y1, x2, y2);
+		for(int i = 0; i < 9; i++) {
+			g2d.draw(new Line2D.Double((i * width/8), 0, (i * width/8), height));
 		}
+		
+		for(int j = 0; j < 5; j++) {
+			g2d.draw(new Line2D.Double(0, (j * height/4), width, (j*height/4)));
+		}
+		
+		graphics.drawString("Monday", (int) (width/8) + 20, (int) height/8);
+		graphics.drawString("Tuesday", (int) (2 * width/8) + 20, (int) height/8);
+		graphics.drawString("Wednesday", (int) (3 * width/8) + 20, (int) height/8);
+		graphics.drawString("Thursday", (int) (4 * width/8) + 20, (int) height/8);
+		graphics.drawString("Friday", (int) (5 * width/8) + 20, (int) height/8);
+		graphics.drawString("Saturday", (int) (6 * width/8) + 20, (int) height/8);
+		graphics.drawString("Sunday", (int) (7 * width/8) + 20, (int) height/8);
+		
+		graphics.drawString("Morning", 10, (int) (height/4) + 56);
+		graphics.drawString("Afternoon", 10, (int) (2 * height/4) + 56);
+		graphics.drawString("Evening", 10, (int) (3 * height/4) + 56);
 		
 		
 		
